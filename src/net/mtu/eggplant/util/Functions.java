50c85341a3a1447f3f75cea301727e28a61ac795
Jon Schewe <jpschewe@mtu.net>
2000-09-25 01:39:08 +0000

    *** empty log message ***

--- src/net/mtu/eggplant/util/Functions.java
+++ src/net/mtu/eggplant/util/Functions.java
@@ -1,3 +1,10 @@
+/*
+  This file is licensed through the GNU public License.  Please read it.
+  Basically you can modify this code as you wish, but you need to distribute
+  the source code for all applications that use this code.
+
+  I'd appreciate comments/suggestions on the code schewe@tcfreenet.org
+*/
 package org.tcfreenet.schewe.utils;
 
 /**
