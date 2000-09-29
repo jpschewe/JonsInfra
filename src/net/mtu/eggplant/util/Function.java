53e5897d3a26d11b32cf4a7ba8cc5689a00ad590
Jon Schewe <jpschewe@mtu.net>
2000-09-29 03:08:51 +0000

    Moved.

---
+++ src/net/mtu/eggplant/util/Function.java
@@ -0,0 +1,16 @@
+/*
+  This file is licensed through the GNU public License.  Please read it.
+  Basically you can modify this code as you wish, but you need to distribute
+  the source code for all applications that use this code.
+
+  I'd appreciate comments/suggestions on the code schewe@tcfreenet.org
+*/
+package org.tcfreenet.schewe.utils;
+
+/**
+   Generic function that takes a single argument and operates on it.
+**/
+public interface Function {
+
+  public void execute(final Object o);
+}
