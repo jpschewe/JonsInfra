1f2a08d3ac86cce38401cef234f5512a381ac19b
Jon Schewe <jpschewe@mtu.net>
2000-10-02 02:57:59 +0000

    Finished implementation.  Delegates calls to default renderers.

--- src/net/mtu/eggplant/util/gui/SmartRenderer.java
+++ src/net/mtu/eggplant/util/gui/SmartRenderer.java
@@ -13,22 +13,25 @@ import org.tcfreenet.schewe.utils.Unique;
 import java.awt.Component;
 
 import javax.swing.ListCellRenderer;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.JList;
 import javax.swing.JTree;
-import javax.swing.UIManager;
-import javax.swing.BorderFactory;
-import javax.swing.JLabel;
 import javax.swing.Icon;
 import javax.swing.border.Border;
 import javax.swing.table.TableCellRenderer;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.tree.TreeCellRenderer;
+import javax.swing.tree.DefaultTreeCellRenderer;
 
 /**
    <p>Class that knows how to render lots of things.  If you want to create a
    renderer, just subclass this SmartRenderer and override the
    getStringValue() method.  For any object that is not a known type toString
-   is used.  Null values are always displayed as "NULL"<p>
+   is used.  Null values are always displayed as "NULL".  This class counts on
+   the fact that the default renderers for list, trable and tree all return a
+   subclass of JLabel.  If that ever changes, this class breaks.<p>
 
    <p>Known types</p>
    <ul>
@@ -38,55 +41,58 @@ import javax.swing.tree.TreeCellRenderer;
    </ul>
    
 **/
-public class SmartRenderer extends JLabel implements ListCellRenderer, TableCellRenderer, TreeCellRenderer {
+public class SmartRenderer implements ListCellRenderer, TableCellRenderer, TreeCellRenderer {
 
   public SmartRenderer() {
-    setOpaque(true);
+    _treeRenderer = new DefaultTreeCellRenderer();
+    _listRenderer = new DefaultListCellRenderer();
+    _tableRenderer = new DefaultTableCellRenderer();
   }
+
+  private DefaultTreeCellRenderer _treeRenderer;
+  private DefaultListCellRenderer _listRenderer;
+  private DefaultTableCellRenderer _tableRenderer;
   
-  public Component getListCellRendererComponent(final JList list,
-                                                final Object value,
-                                                final int index,
-                                                final boolean isSelected,
-                                                final boolean cellHasFocus) {
+  final public Component getListCellRendererComponent(final JList list,
+                                                      final Object value,
+                                                      final int index,
+                                                      final boolean isSelected,
+                                                      final boolean cellHasFocus) {
+
+    final JLabel renderer = (JLabel)_listRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
     final String text;
     if(value == null) {
       text = "NULL";
     } else if (value instanceof Icon) {
-      setIcon((Icon)value);
       text = "";
     } else {
       text = getStringValue(value);
     }
-    setText(text);
-
-    //For accessibility
-    setComponentOrientation(list.getComponentOrientation());
-        
-    if (isSelected) {
-      setBackground(list.getSelectionBackground());
-      setForeground(list.getSelectionForeground());
-    } else {
-      setBackground(list.getBackground());
-      setForeground(list.getForeground());
-    }
-
-    setEnabled(list.isEnabled());
-    setFont(list.getFont());
-    setBorder((cellHasFocus) ? UIManager.getBorder("List.focusCellHighlightBorder") : _emptyBorder);
+    renderer.setText(text);
     
-    return this;
+    return renderer;
   }
 
-  public Component getTableCellRendererComponent(final JTable table,
-                                                 final Object value,
-                                                 final boolean isSelected,
-                                                 final boolean hasFocus,
-                                                 final int row,
-                                                 final int column) {
+  final public Component getTableCellRendererComponent(final JTable table,
+                                                       final Object value,
+                                                       final boolean isSelected,
+                                                       final boolean hasFocus,
+                                                       final int row,
+                                                       final int column) {
 
 
-    return null;
+    final JLabel renderer = (JLabel)_tableRenderer.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+    final String text;
+    if(value == null) {
+      text = "NULL";
+    } else if (value instanceof Icon) {
+      text = "";
+    } else {
+      text = getStringValue(value);
+    }
+    renderer.setText(text);
+    
+    return renderer;
   }
 
   public Component getTreeCellRendererComponent(final JTree tree,
@@ -97,7 +103,18 @@ public class SmartRenderer extends JLabel implements ListCellRenderer, TableCell
                                                 final int row,
                                                 final boolean hasFocus) {
 
-    return null;
+    final JLabel renderer = (JLabel)_treeRenderer.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus);
+    final String text;
+    if(value == null) {
+      text = "NULL";
+    } else if (value instanceof Icon) {
+      text = "";
+    } else {
+      text = getStringValue(value);
+    }
+    renderer.setText(text);
+    
+    return renderer;
   }
 
   /**
@@ -109,11 +126,11 @@ public class SmartRenderer extends JLabel implements ListCellRenderer, TableCell
     } else if(value instanceof Unique) {
       return Long.toString(((Unique)value).getUID());
     } else {
-    return value.toString();
+      return value.toString();
     }
 
   }
 
-  final private static Border _emptyBorder = BorderFactory.createEmptyBorder(1, 1, 1, 1);
+  //final private static Border _emptyBorder = BorderFactory.createEmptyBorder(1, 1, 1, 1);
   
 }
