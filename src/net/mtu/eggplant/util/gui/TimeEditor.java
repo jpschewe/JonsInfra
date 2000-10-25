df31b7b0e329a801e77af3363703301964301bd3
Jon Schewe <jpschewe@mtu.net>
2000-10-25 03:57:39 +0000

    *** empty log message ***

--- src/net/mtu/eggplant/util/gui/TimeEditor.java
+++ src/net/mtu/eggplant/util/gui/TimeEditor.java
@@ -20,48 +20,48 @@ public class TimeEditor extends JPanel {
     
   }
 
-  protected void createFields() {
-    _hourText = new JTextField(4);
-    _minuteText = new JTextField(2);
-    _secondText = new JTextField(2);
-  }
+//   protected void createFields() {
+//     _hourText = new JTextField(4);
+//     _minuteText = new JTextField(2);
+//     _secondText = new JTextField(2);
+//   }
   
-  protected void addFields(boolean hour, boolean minute, boolean second) {
-    if(hour) {
-      addField(HOUR);
-      if(minute || second)
-        addField(TIMESEPARATOR);
-    }
-    if(minute) {
-      addField(MINUTE);
-      if(second)
-        addField(TIMESEPARATOR);
-    }
-    if(second) {
-      addField(second);
-    }
-  }
+//   protected void addFields(boolean hour, boolean minute, boolean second) {
+//     if(hour) {
+//       addField(HOUR);
+//       if(minute || second)
+//         addField(TIMESEPARATOR);
+//     }
+//     if(minute) {
+//       addField(MINUTE);
+//       if(second)
+//         addField(TIMESEPARATOR);
+//     }
+//     if(second) {
+//       addField(second);
+//     }
+//   }
 
-  protected void addField(int field) {
-    switch(field) {
-    case SECOND:
-      _internalPanel.add(_secondText);
-      break;
-    case MINUTE:
-      _internalPanel.add(_minuteText);
-      break;
-    case HOUR:
-      _internalPanel.add(_hourText);
-      break;
-    case TIMESEPARATOR:
-      _internalPanel.add(new JLable(_timeSeparator));
-      break;
-    }
-  }
+//   protected void addField(int field) {
+//     switch(field) {
+//     case SECOND:
+//       _internalPanel.add(_secondText);
+//       break;
+//     case MINUTE:
+//       _internalPanel.add(_minuteText);
+//       break;
+//     case HOUR:
+//       _internalPanel.add(_hourText);
+//       break;
+//     case TIMESEPARATOR:
+//       _internalPanel.add(new JLable(_timeSeparator));
+//       break;
+//     }
+//   }
   
-  private String _timeSeparator = ":";
-  private JTextField _secondText;
-  private JTextField _minuteText;
-  private JTextField _hourText;
+//   private String _timeSeparator = ":";
+//   private JTextField _secondText;
+//   private JTextField _minuteText;
+//   private JTextField _hourText;
   
 }
